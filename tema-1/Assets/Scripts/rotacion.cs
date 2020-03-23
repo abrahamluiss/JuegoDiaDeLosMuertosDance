88196297a40bcf487b8a34777875ce4c12ecf86b
abrahamluiss <abraham105@hotmail.com>
2020-03-22 20:55:51 -0500

    Proyecto 3d iniciado

---
+++ tema-1/Assets/Scripts/rotacion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotacion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(0, 90f * Time.deltaTime, 0); //en cada frame rote 10 grados, al integrar en el tiempo esque en un segundo gire 10 ggraos
+    }
+}
